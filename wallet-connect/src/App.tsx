2b96552973607554aad24cc2d9e134b80251f2cf
ayesha <aishaehsan0@gmail.com>
2024-02-16 16:52:30 +0500

    code cleanup

--- wallet-connect/src/App.tsx
+++ wallet-connect/src/App.tsx
@@ -1,9 +1,4 @@
 function App() {
-  const apiKey = import.meta.env.VITE_API_KEY;
-
-  const baseUrl = import.meta.env.VITE_APP_CHAIN_ID;
-
-  console.log(apiKey, baseUrl);
   return (
     <>
       <h1>Vite + React</h1>
