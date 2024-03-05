9e8d60cdbffe25a015ab152c8c7623dd573f2ada
ayesha <aishaehsan0@gmail.com>
2024-03-05 16:21:49 +0500

    code cleanup

--- wallet-connect/src/App.tsx
+++ wallet-connect/src/App.tsx
@@ -1,10 +1,8 @@
-import { useAuth } from "./hooks/useAuth";
 import { setUpNetwork } from "./utilis/function";
 
 function App() {
   console.log(setUpNetwork("5"));
 
-  console.log(useAuth());
   return (
     <>
       <h1>Vite + React</h1>
