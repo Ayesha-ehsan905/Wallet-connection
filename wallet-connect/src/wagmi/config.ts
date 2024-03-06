e318f9a17e79ddb1517767bc200efca40e2d735b
ayesha <aishaehsan0@gmail.com>
2024-03-06 17:56:40 +0500

    wallet connect

--- wallet-connect/src/wagmi/config.ts
+++ wallet-connect/src/wagmi/config.ts
@@ -1,7 +1,7 @@
 import { http, createConfig } from "wagmi";
 import { injected, metaMask, walletConnect } from "wagmi/connectors";
 import { base, mainnet } from "wagmi/chains";
-const projectId = "<WALLETCONNECT_PROJECT_ID>";
+const projectId = "af2f61a69d6f2e693f1183cd04aeb9a0";
 
 export const config = createConfig({
   chains: [mainnet, base], //mainnet and test net
