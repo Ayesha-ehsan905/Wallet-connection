111f9067f884499dcd7cbb95fc049f546eece885
ayesha <aishaehsan0@gmail.com>
2024-03-20 12:49:56 +0500

    issues

--- wallet-connect/src/wagmi/config.ts
+++ wallet-connect/src/wagmi/config.ts
@@ -1,11 +1,21 @@
 import { http, createConfig } from "wagmi";
-import { injected, metaMask, walletConnect } from "wagmi/connectors";
+import { coinbaseWallet, metaMask, walletConnect } from "wagmi/connectors";
 import { base, mainnet } from "wagmi/chains";
 const projectId = "af2f61a69d6f2e693f1183cd04aeb9a0";
 
 export const config = createConfig({
   chains: [mainnet, base], //mainnet and test net
-  connectors: [injected(), walletConnect({ projectId }), metaMask()],
+  connectors: [
+    coinbaseWallet({
+      appName: "YourAppName",
+      appLogoUrl: "https://example.com/logo.png",
+      darkMode: true,
+      linkAPIUrl: "https://example.com/linkapi",
+      // Add other properties as needed
+    }),
+    walletConnect({ projectId }),
+    metaMask(),
+  ],
   transports: {
     [mainnet.id]: http(),
     [base.id]: http(),
