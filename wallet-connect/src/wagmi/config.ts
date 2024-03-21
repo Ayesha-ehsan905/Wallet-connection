2283f6be3268554afca0e543bbda72262dad92e0
ayesha <aishaehsan0@gmail.com>
2024-03-21 11:52:03 +0500

    marketplace

--- wallet-connect/src/wagmi/config.ts
+++ wallet-connect/src/wagmi/config.ts
@@ -1,21 +1,11 @@
 import { http, createConfig } from "wagmi";
-import { coinbaseWallet, metaMask, walletConnect } from "wagmi/connectors";
+import { metaMask } from "wagmi/connectors";
 import { base, mainnet } from "wagmi/chains";
-const projectId = "af2f61a69d6f2e693f1183cd04aeb9a0";
+// const projectId = "af2f61a69d6f2e693f1183cd04aeb9a0";
 
 export const config = createConfig({
   chains: [mainnet, base], //mainnet and test net
-  connectors: [
-    coinbaseWallet({
-      appName: "YourAppName",
-      appLogoUrl: "https://example.com/logo.png",
-      darkMode: true,
-      linkAPIUrl: "https://example.com/linkapi",
-      // Add other properties as needed
-    }),
-    walletConnect({ projectId }),
-    metaMask(),
-  ],
+  connectors: [metaMask()],
   transports: {
     [mainnet.id]: http(),
     [base.id]: http(),
